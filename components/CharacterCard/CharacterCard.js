bed37ed7e7e10a7243a5484a9a994e1e2462d034
Steffen Anslinger <steffen.anslinger@hotmail.com>
2023-11-29 15:51:13 +0100

    add function fetchData to CharacterCard.js

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,27 +1,36 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
 
 export function CharacterCard() {
+  const data = fetchData();
   cardContainer.innerHTML += `<li class="card">
     <div class="card__image-container">
       <img
         class="card__image"
-        src="${url}"
-        alt="${name}"
+        src="${data.url}"
+        alt="${data.name}"
       />
       <div class="card__image-gradient"></div>
     </div>
     <div class="card__content">
-      <h2 class="card__title">${name}</h2>
+      <h2 class="card__title">${data.name}</h2>
       <dl class="card__info">
         <dt class="card__info-title">Status</dt>
-        <dd class="card__info-description">${status}</dd>
+        <dd class="card__info-description">${data.status}</dd>
         <dt class="card__info-title">Type</dt>
-        <dd class="card__info-description">${type}</dd>
+        <dd class="card__info-description">${data.type}</dd>
         <dt class="card__info-title">Occurrences</dt>
-        <dd class="card__info-description">${episode.length}</dd>
+        <dd class="card__info-description">25</dd>
       </dl>
     </div>
   </li>`;
 }
 
 CharacterCard();
+
+async function fetchData() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+  return data.results;
+}
+
+fetchData();
