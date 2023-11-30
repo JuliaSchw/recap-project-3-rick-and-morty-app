0a10601ca3e256faf9cee00a52802363c2004e51
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-11-30 11:08:07 +0100

    add backticks to index.js

--- index.js
+++ index.js
@@ -10,27 +10,48 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 // Data
+// Why do we get the data for maxPage from the URL even though we only returned data.results?
 let maxPage = 1;
 let page = 1;
 const searchQuery = "";
 
-const characters = await fetchData();
-
 async function fetchData() {
   const response = await fetch(
-    "https://rickandmortyapi.com/api/character?page=${page}"
+    `https://rickandmortyapi.com/api/character?page=${page}`
   );
   const data = await response.json();
   maxPage = data.info.pages;
   return data.results;
 }
-pagination.textContent = `${page} / ${maxPage}`;
-characters.forEach((character) => {
-  CharacterCard(
-    character.image,
-    character.name,
-    character.status,
-    character.type,
-    character.episode.length
-  );
+
+async function fetchCharacters() {
+  const characters = await fetchData();
+
+  cardContainer.innerHTML = ""; // Clear existing cards
+
+  characters.forEach((character) => {
+    CharacterCard(
+      character.image,
+      character.name,
+      character.status,
+      character.type,
+      character.episode.length
+    );
+  });
+  pagination.textContent = `${page} / ${maxPage}`;
+}
+fetchCharacters();
+
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters();
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters();
+  }
 });
