abe877ab12401dc8c4aaecbee126d66c122cae97
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-11-30 10:37:40 +0100

    Insert Pagination to index.js

--- index.js
+++ index.js
@@ -9,20 +9,22 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
-
-// States
-const maxPage = 1;
-const page = 1;
+// Data
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
 const characters = await fetchData();
 
 async function fetchData() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(
+    "https://rickandmortyapi.com/api/character?page=${page}"
+  );
   const data = await response.json();
+  maxPage = data.info.pages;
   return data.results;
 }
-
+pagination.textContent = `${page} / ${maxPage}`;
 characters.forEach((character) => {
   CharacterCard(
     character.image,
